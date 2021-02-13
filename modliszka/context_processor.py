4fe196054d52f2c68c22fd3b3253fef91448a09e
cooklee <sbbogus@gmail.com>
2021-02-13 11:21:36 +0100

    add models

--- modliszka/context_processor.py
+++ modliszka/context_processor.py
@@ -2,4 +2,3 @@ from datetime import datetime
 
 def get_time(request):
     return {'date':datetime.now()}
-
--- modliszka/models.py
+++ modliszka/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.FloatField()
+
+
+class Card(models.Model):
+    products = models.ManyToManyField(Product)
+    client = models.OneToOneField(Client, on_delete=models.CASCADE)
+
+class Order(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product)
+    date = models.DateField(auto_now_add=True)
